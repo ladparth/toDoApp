e0acf823bb817f0810b9d1e7982c07a4f35dd987
ladparth <81832445+ladparth@users.noreply.github.com>
2022-07-26 22:01:26 -0400

    minor changes

--- components/TaskSection.js
+++ components/TaskSection.js
@@ -3,9 +3,8 @@ import React from 'react'
 import Task from './Task'
 import { Collapse,CollapseBody,CollapseHeader } from 'accordion-collapse-react-native'
 
-const TaskSection = ({title,taskArray,status,taskFunction}) => {
 
-  const isExpanded = true
+const TaskSection = ({title,taskArray,status,taskFunction,expanded,fnToggle}) => {
   
   const taskList = taskArray.filter((item)=>{
     return item.isComplete === status
@@ -13,12 +12,12 @@ const TaskSection = ({title,taskArray,status,taskFunction}) => {
 
   return (
     <View style ={styles.taskWrapper}>
-      <Collapse isExpanded={isExpanded}>
+      <Collapse isExpanded={expanded} onToggle={()=>fnToggle()}>
         <CollapseHeader style={styles.header}>
           <Text style={styles.sectionTitle}>{`${title}(${taskList.length}) >`}</Text>
         </CollapseHeader>
         <CollapseBody style={styles.items}>
-        {
+                {
                 taskList.map((item)=>{
                   return (
                     <TouchableOpacity style={styles.completedTask} key={item.id} 
